94f929b6c4f114ca0bf9ac335c89d96fd9d1330e
among5094 <106166621+among5094@users.noreply.github.com>
2024-05-04 12:27:50 +0900

    Add files via upload

---
+++ UserAccount.java
@@ -0,0 +1,29 @@
+package com.example.firebaseregister;
+
+public class UserAccount {
+
+    public UserAccount(){
+
+    }
+
+    public  String getIdToken(){ return idToken; }
+
+    public void setIdToken(String idToken){
+        this.idToken=idToken;
+    }
+
+    private String idToken;
+
+    public String getEmailId() { return emailId; }
+
+    public void setEmailId(String emailId) { this.emailId=emailId; }
+
+    private String emailId;
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password=password; }
+    private String password;
+
+
+}
